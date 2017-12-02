e0ab2ab23d6131fed98c845286bb171105533754
Luca Versari <veluca93@gmail.com>
2017-12-02 19:49:26 +0100

    Create task_maker.py with all the necessary modules.

---
+++ bindings/task_maker.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from bindings import core
+from bindings import execution
+from bindings import file_id
--- python/test.py
+++ python/test.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-import bindings.core
-import bindings.execution
-import bindings.file_id
+from bindings.task_maker import core
+from bindings.task_maker import file_id
+from bindings.task_maker import execution
 import tempfile
 
 with tempfile.NamedTemporaryFile() as tmp:
     tmp.write(b"Lorem ipsum dolor sit amet.")
     tmp.seek(0)
 
-    core = bindings.core.core()
-    f = core.load_file("tmp file", tmp.name)
-    ex = core.add_execution("test", "/bin/cat", ["foobar"])
+    exec_core = core.core()
+    f = exec_core.load_file("tmp file", tmp.name)
+    ex = exec_core.add_execution("test", "/bin/cat", ["foobar"])
     ex.input("foobar", f)
-    assert core.run()
+    assert exec_core.run()
     assert ex.stdout().contents(100) == "Lorem ipsum dolor sit amet."
     assert ex.stderr().contents(100) == ""
