f59ed0663dc6e40b61931323e153e33ced7e534a
Luca Versari <veluca93@gmail.com>
2017-12-21 01:01:32 +0100

    Fix modules imports when running.

--- tools/python_gateway.sh
+++ tools/python_gateway.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 PYTHON_DIR="$(dirname $0)/../../python_repo/python_3_6_files"
-export LD_LIBRARY_PATH=$PYTHON_DIR/lib
+export LD_LIBRARY_PATH="$PYTHON_DIR/lib"
+export PYTHONHOME="$PYTHON_DIR"
 export PYTHONPATH="$(dirname $0)/.."
 $PYTHON_DIR/bin/python3 "$@"
