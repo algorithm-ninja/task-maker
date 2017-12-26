c40f7ae5024ccb0b6e8681d590069e02b03484bd
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2017-12-26 19:18:12 +0100

    All the paths are now absolute

--- python/task_maker.py
+++ python/task_maker.py
@@ -4,6 +4,8 @@ import argparse
 import os
 
 from proto.manager_pb2 import ALL, GENERATION, NOTHING  # CacheMode
+from proto.manager_pb2 import EvaluateTaskRequest
+from proto.task_pb2 import Task, Subtask, TestCase, SourceFile, GraderInfo
 
 from python.curses_ui import CursesUI
 from python.italian_format import get_request
@@ -21,6 +23,66 @@ CACHES = {
     "generation": GENERATION,
     "nothing": NOTHING
 }
+
+
+def absolutize_path(path: str) -> str:
+    if os.path.isabs(path):
+        return path
+    return os.path.join(os.getcwd(), path)
+
+
+def absolutize_source_file(source_file: SourceFile) -> None:
+    source_file.path = absolutize_path(source_file.path)
+    for dependency in source_file.deps:
+        dependency.path = absolutize_path(dependency.path)
+
+
+def absolutize_testcase(testcase: TestCase) -> None:
+    if testcase.HasField("generator"):
+        absolutize_source_file(testcase.generator)
+    if testcase.HasField("validator"):
+        absolutize_source_file(testcase.validator)
+    if testcase.input_file:
+        testcase.input_file = absolutize_path(testcase.input_file)
+    if testcase.output_file:
+        testcase.output_file = absolutize_path(testcase.output_file)
+
+
+def absolutize_subtask(subtask: Subtask) -> None:
+    for testcase in subtask.testcases:
+        absolutize_testcase(testcase)
+
+
+def absolutize_grader_info(info: GraderInfo) -> None:
+    for dependency in info.files:
+        dependency.path = absolutize_path(dependency.path)
+
+
+def absolutize_task(task: Task) -> None:
+    for subtask in task.subtasks:
+        absolutize_subtask(subtask)
+    if task.HasField("official_solution"):
+        absolutize_source_file(task.official_solution)
+    for info in task.grader_info:
+        absolutize_grader_info(info)
+    if task.HasField("checker"):
+        absolutize_source_file(task.checker)
+
+
+def absolutize_request(request: EvaluateTaskRequest) -> None:
+    absolutize_task(request.task)
+    for solution in request.solutions:
+        absolutize_source_file(solution)
+    request.store_dir = absolutize_path(request.store_dir)
+    request.temp_dir = absolutize_path(request.temp_dir)
+    for testcase in request.write_inputs_to:
+        request.write_inputs_to[testcase] =\
+            absolutize_path(request.write_inputs_to[testcase])
+    for testcase in request.write_outputs_to:
+        request.write_outputs_to[testcase] =\
+            absolutize_path(request.write_outputs_to[testcase])
+    if request.write_checker_to:
+        request.write_checker_to = absolutize_path(request.write_checker_to)
 
 
 def _validate_num_cores(num: str) -> int:
@@ -106,6 +168,7 @@ def main() -> None:
         return
 
     request = get_request(args)
+    absolutize_request(request)
     print(request)
 
 
