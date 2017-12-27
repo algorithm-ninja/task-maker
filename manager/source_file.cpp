59b448a99f4a420cf530e209af6780bef25e603f
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2017-12-27 09:44:26 +0100

    Implemented source_file in C++

--- manager/source_file.cpp
+++ manager/source_file.cpp
@@ -5,8 +5,87 @@ namespace manager {
 // static
 std::unique_ptr<SourceFile> SourceFile::FromProto(
     EventQueue* queue, core::Core* core, const proto::SourceFile& source,
-    bool fatal_failures) {
-  // return absl::make_unique<...>();
-  return nullptr;
+    const proto::GraderInfo& grader, bool fatal_failures) {
+  switch (source.language()) {
+    case proto::CPP:
+    case proto::C:
+    case proto::PASCAL:
+      return absl::make_unique<CompiledSourceFile>(queue, core, source,
+                                                   grader, fatal_failures);
+    default:
+      return absl::make_unique<NotCompiledSourceFile>(queue, core, source,
+                                                      fatal_failures);
+  }
 }
+
+CompiledSourceFile::CompiledSourceFile(
+    EventQueue *queue, core::Core *core, const proto::SourceFile &source,
+    const proto::GraderInfo& grader, bool fatal_failures)
+    : SourceFile(core, queue, fatal_failures) {
+  std::string compiler;
+  std::vector<std::string> args;
+
+  switch (source.language()) {
+    case proto::CPP:
+      compiler = "/usr/bin/g++";
+      args = {"-O2", "-std=c++14", "-DEVAL", "-Wall", "-o", "compiled",
+              source.path()};
+      break;
+    case proto::C:
+      compiler = "/usr/bin/gcc";
+      args = {"-O2", "-std=c11", "-DEVAL", "-Wall", "-o", "compiled",
+              source.path()};
+      break;
+    case proto::PASCAL:
+      compiler = "/usr/bin/fpc";
+      args = {"-dEVAL", "-XS", "-O2", "-ocompiled", source.path()};
+      break;
+    default:
+      throw std::domain_error("Cannot compiled " + source.path() + " because "
+          "unknown compilation command");
+  }
+  for (auto dep : grader.files())
+    args.push_back(dep.name());
+
+  compilation_ = core->AddExecution(
+      "Compilation of " + source.path(), compiler, args);
+
+  for (auto dep : source.deps())
+    compilation_->Input(dep.name(), core->LoadFile(dep.name(), dep.path()));
+  for (auto dep : grader.files())
+    compilation_->Input(dep.name(), core->LoadFile(dep.name(), dep.path()));
+
+  compiled_ = compilation_->Output("compiled", "Compiled file of " +
+      source.path());
+  queue->CompilationWaiting(source.path());
+}
+
+core::Execution* CompiledSourceFile::execute(
+    const std::string& description, const std::vector<std::string>& args) {
+  core::Execution* execution = core_->AddExecution(description, "program", args);
+  execution->Input("program", compiled_);
+  return execution;
+}
+
+NotCompiledSourceFile::NotCompiledSourceFile(EventQueue *queue,
+                                             core::Core *core,
+                                             const proto::SourceFile &source,
+                                             bool fatal_failures)
+    : SourceFile(core, queue, fatal_failures) {
+  core_ = core;
+  queue_ = queue;
+  for (auto dep : source.deps())
+    runtime_deps_.push_back(core->LoadFile(dep.name(), dep.path()));
+  program_ = core->LoadFile(source.path(), source.path());
+}
+
+core::Execution* NotCompiledSourceFile::execute(
+    const std::string& description, const std::vector<std::string>& args) {
+  core::Execution* execution = core_->AddExecution(description, "program", args);
+  execution->Input("program", program_);
+  for (auto dep : runtime_deps_)
+    execution->Input(dep->Description(), dep);
+  return execution;
+}
+
 }  // namespace manager
--- manager/source_file.hpp
+++ manager/source_file.hpp
@@ -1,6 +1,7 @@
 #ifndef MANAGER_SOURCE_FILE_HPP
 #define MANAGER_SOURCE_FILE_HPP
 
+#include <vector>
 #include "core/core.hpp"
 #include "core/execution.hpp"
 #include "manager/event_queue.hpp"
@@ -9,10 +10,9 @@
 namespace manager {
 class SourceFile {
  public:
-  static std::unique_ptr<SourceFile> FromProto(EventQueue* queue,
-                                               core::Core* core,
-                                               const proto::SourceFile& source,
-                                               bool fatal_failures = false);
+  static std::unique_ptr<SourceFile> FromProto(
+      EventQueue* queue, core::Core* core, const proto::SourceFile& source,
+      const proto::GraderInfo& grader, bool fatal_failures = false);
   virtual core::Execution* execute(const std::string& description,
                                    const std::vector<std::string>& args) = 0;
   virtual ~SourceFile() = default;
@@ -20,6 +20,47 @@ class SourceFile {
   SourceFile(SourceFile&&) = delete;
   SourceFile& operator=(const SourceFile&) = delete;
   SourceFile& operator=(SourceFile&&) = delete;
+
+  bool FatalFailures() const { return fatal_failures_; }
+
+ protected:
+  SourceFile(core::Core* core, EventQueue* queue, bool fatal_failures)
+    : core_(core), queue_(queue), fatal_failures_(fatal_failures) {};
+
+  core::Core* core_;
+  EventQueue* queue_;
+  bool fatal_failures_;
+};
+
+class CompiledSourceFile : public SourceFile {
+ public:
+  CompiledSourceFile(EventQueue* queue,
+                     core::Core* core,
+                     const proto::SourceFile& source,
+                     const proto::GraderInfo& grader,
+                     bool fatal_failures = false);
+
+  core::Execution* execute(const std::string& description,
+                           const std::vector<std::string>& args);
+
+ protected:
+  core::Execution* compilation_;
+  core::FileID* compiled_;
+};
+
+class NotCompiledSourceFile : public SourceFile {
+ public:
+  NotCompiledSourceFile(EventQueue* queue,
+                        core::Core* core,
+                        const proto::SourceFile& source,
+                        bool fatal_failures = false);
+
+  core::Execution* execute(const std::string& description,
+                           const std::vector<std::string>& args);
+
+ protected:
+  std::vector<core::FileID*> runtime_deps_;
+  core::FileID* program_;
 };
 
 }  // namespace manager
