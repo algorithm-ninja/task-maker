436b6e9cc956120e8eb5267cf556c10fb246ab13
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-02-03 12:20:44 +0100

    Fixed artifacts in tags build

--- release.sh
+++ release.sh
@@ -8,15 +8,21 @@ TEMPDIR=/tmp
 bazel build -c opt //python:task_maker
 mkdir -p $TEMPDIR/task_maker
 cp -r bazel-bin/python/task_maker.runfiles bazel-bin/python/task_maker -L $TEMPDIR/task_maker
-cd $TEMPDIR/task_maker
+
+pushd $TEMPDIR/task_maker
+
 chmod u+w -R .
 for dep in $(cd task_maker.runfiles/; ls -d pypi*)
 do
   rm -rf task_maker.runfiles/$dep
   ln -s oii_task_maker/external/$dep task_maker.runfiles
 done
-find . -name '*.pyc' -delete
+find . -type f -name '*.pyc' -delete
 rm -rf task_maker.runfiles/oii_task_maker/_solib_k8
-find . -name '*.so' | xargs strip -s || true
-find . -name 'manager' | xargs strip -s || true
+find . -type f -name '*.so' | xargs strip -s || true
+find . -type f -name 'manager' | xargs strip -s || true
 find . -type f -print0 | xargs -0 chmod u-w
+
+popd
+
+mv $TEMPDIR/task_maker ./task_maker-$CI_COMMIT_TAG
