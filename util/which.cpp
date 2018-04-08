6a7699c855690e0a606e79b7e78a9cb2e9ad994c
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-04-08 21:08:30 +0200

    Changed behaviour of which with the cache

--- util/which.cpp
+++ util/which.cpp
@@ -1,16 +1,16 @@
+#include "util/which.hpp"
+#include <sys/stat.h>
 #include "absl/strings/str_split.h"
 #include "cstdlib"
 #include "string"
 #include "unordered_map"
 #include "util/file.hpp"
-#include "util/which.hpp"
-#include <sys/stat.h>
 
 namespace {
 std::unordered_map<std::string, std::string> cmd_cache;
 
 bool file_exists(const std::string& path) {
-  struct stat buffer{};
+  struct stat buffer {};
   return stat(path.c_str(), &buffer) == 0;
 }
 }  // namespace
@@ -21,21 +21,14 @@ std::string which(const std::string& cmd, bool use_cache) {
   static const std::string path = std::getenv("PATH");
   static const std::vector<std::string> dirs = absl::StrSplit(path, ":");
 
-  if (use_cache && cmd_cache.count(cmd) > 0)
-    return cmd_cache[cmd];
+  if (use_cache && cmd_cache.count(cmd) > 0) return cmd_cache[cmd];
 
   for (const std::string& dir : dirs) {
     std::string fullpath = util::File::JoinPath(dir, cmd);
-    if (file_exists(fullpath)) {
-      if (use_cache)
-        cmd_cache[cmd] = fullpath;
-      return fullpath;
-    }
+    if (file_exists(fullpath)) return cmd_cache[cmd] = fullpath;
   }
 
-  if (use_cache)
-    cmd_cache[cmd] = "";
-  return "";
+  return cmd_cache[cmd] = "";
 }
 
 }  // namespace util
