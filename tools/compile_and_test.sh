9690462d8e8196b68cc4705e2572aa31f0b8baa3
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-06-05 23:28:23 +0200

    Testing...

--- tools/compile_and_test.sh
+++ tools/compile_and_test.sh
@@ -4,7 +4,9 @@ cmake -H. -Bbuild -DHUNTER_ENABLED=OFF
 cmake --build build
 # yee sudo setup.py install!! it's on a container, so... ¯\_(ツ)_/¯
 sudo python build/python/setup.py install
-( cd build && ctest -VV )
+#( cd build && ctest -VV )
+GLOG_logtostderr=1 GLOG_v=2 ./build/cpp/task-maker -mode manager -port 7071 &
+env PYTHONPATH=./build/python python3 ./build/python/task_maker/tests/with_st.py
 
 for f in $(find /tmp -name "task-maker.*"); do
     echo "-------------- $f ------------------"
