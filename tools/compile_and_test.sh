9498d727e91678145bbfc50a50b919fe06d718db
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-06-06 16:16:44 +0200

    Added compilation flag to enable or disable dangerous sandbox cleaning

--- tools/compile_and_test.sh
+++ tools/compile_and_test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-cmake -H. -Bbuild -DHUNTER_ENABLED=OFF
+if [ "$CI" != "true" ]; then
+    echo "This script is supposed to run on the CI, it may break your system"
+    exit 1
+fi
+
+cmake -H. -Bbuild -DHUNTER_ENABLED=OFF -DTRAVIS=ON
 cmake --build build
 # yee sudo setup.py install!! it's on a container, so... ¯\_(ツ)_/¯
 sudo python build/python/setup.py install
-#( cd build && ctest -VV )
-GLOG_logtostderr=1 GLOG_v=2 ./build/cpp/task-maker -mode manager -port 7071 &
-env PYTHONPATH=./build/python python3 ./build/python/task_maker/tests/with_st.py
+( cd build && ctest -VV )
 
 for f in $(find /tmp -name "task-maker.*"); do
     echo "-------------- $f ------------------"
