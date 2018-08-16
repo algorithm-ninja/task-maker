12d4dd59b84caa564164b2d923c339cff7173412
Luca Versari <veluca93@gmail.com>
2018-08-16 12:11:01 +0200

    Skeleton implementation of Execution

--- cpp/server/server.cpp
+++ cpp/server/server.cpp
@@ -5,12 +5,75 @@
 #include <string>
 
 namespace server {
+kj::Promise<void> Execution::setExecutablePath(
+    SetExecutablePathContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::setExecutable(SetExecutableContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::setStdin(SetStdinContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::addInput(AddInputContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::setArgs(SetArgsContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::disableCache(DisableCacheContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::makeExclusive(MakeExclusiveContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::setLimits(SetLimitsContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::stdout(StdoutContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::stderr(StderrContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::output(OutputContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::notifyStart(NotifyStartContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> Execution::getResults(GetResultContext context) {
+  return kj::READY_NOW;
+}
+
+kj::Promise<void> FrontendContext::provideFile(ProvideFileContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> FrontendContext::addExecution(AddExecutionContext context) {
+  // TODO: see Server::registerFrontend
+  context.getResults().setExecution(
+      kj::heap<Execution>(dispatcher_, last_file_id_, file_info_,
+                          context.getParams().getDescription()));
+  return kj::READY_NOW;
+}
+kj::Promise<void> FrontendContext::startEvaluation(
+    StartEvaluationContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> FrontendContext::getFileContents(
+    GetFileContentsContext context) {
+  return kj::READY_NOW;
+}
+kj::Promise<void> FrontendContext::stopEvaluation(
+    StopEvaluationContext context) {
+  return kj::READY_NOW;
+}
 
 kj::Promise<void> Server::registerFrontend(RegisterFrontendContext context) {
   // TODO: here, we assume that capnproto keeps the FrontendContext alive
   // as long as the client still call its methods, which seems reasonable but
   // could not be the case.
-  context.getResults().setContext(kj::heap<FrontendContext>(*this));
+  context.getResults().setContext(kj::heap<FrontendContext>(dispatcher_));
   return kj::READY_NOW;
 }
 
--- cpp/server/server.hpp
+++ cpp/server/server.hpp
@@ -3,34 +3,72 @@
 
 #include "capnp/server.capnp.h"
 #include "server/dispatcher.hpp"
+#include "util/sha256.hpp"
+
+#include <capnp/message.h>
+#include <unordered_map>
 
 namespace server {
 
-class Server;
+namespace detail {
+struct FileInfo {
+  bool executable;
+  kj::PromiseFulfillerPair<void> promise;
+  util::SHA256_t hash = util::SHA256_t::ZERO;
+};
+};  // namespace detail
+
+class Execution : public capnproto::Execution::Server {
+ public:
+  KJ_DISALLOW_COPY(Execution);
+  Execution(server::Dispatcher& dispatcher, uint32_t& last_file_id,
+            std::unordered_map<uint32_t, detail::FileInfo>& file_info,
+            std::string description)
+      : dispatcher_(dispatcher),
+        last_file_id_(last_file_id),
+        file_info_(file_info),
+        description_(description) {}
+
+  kj::Promise<void> setExecutablePath(SetExecutablePathContext context);
+  kj::Promise<void> setExecutable(SetExecutableContext context);
+  kj::Promise<void> setStdin(SetStdinContext context);
+  kj::Promise<void> addInput(AddInputContext context);
+  kj::Promise<void> setArgs(SetArgsContext context);
+  kj::Promise<void> disableCache(DisableCacheContext context);
+  kj::Promise<void> makeExclusive(MakeExclusiveContext context);
+  kj::Promise<void> setLimits(SetLimitsContext context);
+  kj::Promise<void> stdout(StdoutContext context);
+  kj::Promise<void> stderr(StderrContext context);
+  kj::Promise<void> output(OutputContext context);
+  kj::Promise<void> notifyStart(NotifyStartContext context);
+  kj::Promise<void> getResults(GetResultContext context);
+
+ private:
+  server::Dispatcher& dispatcher_;
+  uint32_t& last_file_id_;
+  std::unordered_map<uint32_t, detail::FileInfo>& file_info_;
+  std::string description_;
+  capnp::MallocMessageBuilder builder_;
+  capnproto::Request::Builder request_ =
+      builder_.initRoot<capnproto::Request>();
+  std::vector<std::pair<std::string, int>> inputs;
+  std::vector<std::pair<std::string, int>> outputs;
+};
 
 class FrontendContext : public capnproto::FrontendContext::Server {
  public:
   KJ_DISALLOW_COPY(FrontendContext);
-  FrontendContext(server::Server& server) : server_(server) {}
-  kj::Promise<void> provideFile(ProvideFileContext context) {
-    return kj::READY_NOW;
-  }
-  kj::Promise<void> addExecution(AddExecutionContext context) {
-    return kj::READY_NOW;
-  }
-  kj::Promise<void> startEvaluation(StartEvaluationContext context) {
-    return kj::READY_NOW;
-  }
-  kj::Promise<void> getFileContents(GetFileContentsContext context) {
-    return kj::READY_NOW;
-  }
-  kj::Promise<void> stopEvaluation(StopEvaluationContext context) {
-    return kj::READY_NOW;
-  }
+  FrontendContext(server::Dispatcher& dispatcher) : dispatcher_(dispatcher) {}
+  kj::Promise<void> provideFile(ProvideFileContext context);
+  kj::Promise<void> addExecution(AddExecutionContext context);
+  kj::Promise<void> startEvaluation(StartEvaluationContext context);
+  kj::Promise<void> getFileContents(GetFileContentsContext context);
+  kj::Promise<void> stopEvaluation(StopEvaluationContext context);
 
  private:
-  server::Server& server_;
+  server::Dispatcher& dispatcher_;
   uint32_t last_file_id_;
+  std::unordered_map<uint32_t, detail::FileInfo> file_info_;
 };
 
 class Server : public capnproto::MainServer::Server {
