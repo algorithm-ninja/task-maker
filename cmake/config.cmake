6a4b272c1dbc1c5a7af36c6820445967194e6a9b
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-08-19 14:49:15 +0200

    Fixed with -fPIC for Capnproto

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,6 +1,2 @@
-# enable -fPIC to ZLib because gRPC needs it to link
-hunter_config(ZLIB VERSION ${HUNTER_ZLIB_VERSION}
+hunter_config(CapnProto VERSION ${HUNTER_CapnProto_VERSION}
               CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=TRUE)
-
-hunter_config(c-ares VERSION ${HUNTER_c-ares_VERSION}
-              CMAKE_ARGS CARES_STATIC=ON CARES_SHARED=OFF CARES_STATIC_PIC=ON)
