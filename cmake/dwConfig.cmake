cad71bd987d2ebbe290a65aa8e90768f8ac3971e
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-09-02 12:52:34 +0200

    libdw is now optional

--- cmake/dwConfig.cmake
+++ cmake/dwConfig.cmake
@@ -4,14 +4,13 @@ find_library(
 )
 mark_as_advanced(dw_LIBRARY)
 
-add_library(dw UNKNOWN IMPORTED)
-set_target_properties(dw PROPERTIES
-    IMPORTED_LOCATION ${dw_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(
-  dw
-  REQUIRED_VARS
-  dw_LIBRARY
-)
+if (NOT dw_LIBRARY)
+  message(STATUS "libdw not found")
+  set(dw_FOUND FALSE)
+else()
+  message(STATUS "Found dw: ${dw_LIBRARY}")
+  add_library(dw UNKNOWN IMPORTED)
+  set_target_properties(dw PROPERTIES
+      IMPORTED_LOCATION ${dw_LIBRARY})
+  set(dw_FOUND TRUE)
+endif ()
