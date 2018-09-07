e51367e7089acc4280d734c7f558f5305dee1848
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-09-07 20:12:31 +0200

    Now it's not possible to have dependency loops in python

--- python/languages/python.py
+++ python/languages/python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os.path
 import re
-from typing import List
+from typing import List, Set
 
 from task_maker.languages import Language, LanguageManager, Dependency, \
     make_unique
@@ -9,21 +9,29 @@ from task_maker.languages import Language, LanguageManager, Dependency, \
 PY_IMPORT = re.compile('import +(.+)|from +(.+) +import')
 
 
-def find_python_dependency(filename: str) -> List[Dependency]:
-    scope = os.path.dirname(filename)
+def _extract_imports(filename: str) -> List[str]:
     with open(filename) as file:
         content = file.read()
     imports = PY_IMPORT.findall(content)
+    return [imp for imps in imports for imp in (imps[0] or imps[1]).split(",")]
+
+
+def find_python_dependency(filename: str) -> List[Dependency]:
+    scope = os.path.dirname(filename)
     dependencies = []  # type: List[Dependency]
-    for imp in imports:
-        import_files = imp[0] or imp[1]
-        for import_file in import_files.split(","):
-            file_path = os.path.join(scope, import_file.strip() + ".py")
-            basename = os.path.basename(file_path)
-            if os.path.exists(file_path):
-                dependency = Dependency(basename, file_path)
-                dependencies += [dependency]
-                dependencies += find_python_dependency(file_path)
+    pending = {filename}  # type: Set[str]
+    done = {}  # type: Set[str]
+    while pending:
+        path = pending.pop()
+        imports = _extract_imports(path)
+        done.add(path)
+        for imp in imports:
+            imp_path = os.path.join(scope, imp + ".py")
+            if os.path.exists(imp_path) and imp_path not in done:
+                basename = os.path.basename(imp_path)
+                dependencies.append(Dependency(basename, imp_path))
+                pending.add(imp_path)
+
     return dependencies
 
 
