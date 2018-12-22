188eb328ccce5a3fe39a4ab365f56198351bb570
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-12-22 14:33:21 +0100

    Skipped osx build in tags and linting only changed files

--- tools/compile_and_lint.sh
+++ tools/compile_and_lint.sh
@@ -6,4 +6,4 @@ python3 -m venv /tmp/venv --system-site-packages
 . /tmp/venv/bin/activate
 cmake -H. -Bbuild -DHUNTER_ENABLED=OFF -DTRAVIS=ON
 cmake --build build
-./hooks/lint-all
+./hooks/lint-head
