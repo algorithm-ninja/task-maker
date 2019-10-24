c84eb4a8d880003e45cc36a991bb307aded97591
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2019-10-24 15:17:13 +0200

    Removed python3.7 dependencies

--- python/languages/java.py
+++ python/languages/java.py
@@ -21,8 +21,8 @@ class LanguageJava(CompiledLanguage):
         GRAAL_PATH = os.getenv("TM_GRAAL_PATH") or "/usr/lib/jvm/java-8-graal/bin/"
         main_class = "grader" if "grader.java" in source_filenames else unit_name
         commands = [
-            f"{GRAAL_PATH}javac {' '.join(source_filenames)}",
-            f"{GRAAL_PATH}native-image {main_class} {exe_name}",
+            "{}javac {}".format(GRAAL_PATH, " ".join(source_filenames)),
+            "{}native-image {} {}".format(GRAAL_PATH, main_class, exe_name),
         ]
         return CommandType.SYSTEM, ["sh", "-c", "&&".join(commands)]
 
